7c984ba1af4ff9160db79bc2f92b4db1304b3e6c
Fathia Tafesh <157665638+fayotic@users.noreply.github.com>
2025-11-19 22:15:23 -0600

    Calculated entropy of the set of images

--- Encryption/Encrypt.py
+++ Encryption/Encrypt.py
@@ -12,7 +12,7 @@ import hashlib
 #Starts up OpenCV webcam, takes photos from the webcam that will be used to collect data from. 
 def createImages():
         #Set number of photos taken
-        numberOfPhotos = 10
+        numberOfPhotos = 20
         #Image counter used for file naming process/debugging
         counter = 0
         #Timer for photos being taken after each iteration
@@ -27,8 +27,8 @@ def createImages():
         #Captures video
         cap = cv.VideoCapture(0)
 
-        desired_width = 1280
-        desired_height = 720
+        desired_width = 480
+        desired_height = 640
 
         cap.set(cv.CAP_PROP_FRAME_WIDTH, desired_width)
         cap.set(cv.CAP_PROP_FRAME_HEIGHT, desired_height)
@@ -78,8 +78,8 @@ def createImages():
         cv.destroyAllWindows
         
         
-#Takes images from captured_images directory and gets random data and encrypts it with SHA-256 Hash
-#NOTE: the image dimensions are 480 by 640!!
+#Takes images from captured_images directory and gets random data 
+#NOTE: 
 def analyzeImages():
         counter = 0 #To label images
         p_value = [] #To store all random pixel values
@@ -101,20 +101,49 @@ def analyzeImages():
                 rgb_values.append(greenValue)
                 rgb_values.append(blueValue)
                 stringRGBvalue = "".join(str(i) for i in rgb_values ) 
-            
+                
+            print("Raw Key: ")
             print(stringRGBvalue)
+            string_len = len(stringRGBvalue)
+            print("Key Length: ")
+            print (string_len)
+            entropy()
             return stringRGBvalue
-                
-                
+           
                 
 
         else:
              print("No images in directory")
 
+def entropy():
+       save_folder = "captured_images"
+       directory = os.listdir(save_folder)
+       entropy = 0.0
+       counter = 0
+       if len(directory) > 0:
+            for files in directory:
+                  img_name = os.path.join(save_folder, f"opencv_frame_{counter}.png") 
+                  img = cv.imread(img_name, cv.IMREAD_GRAYSCALE) #Convert the image to greyscale
+                  image_pixels = img.size #Size of image
+                  histogram = cv.calcHist([img], [0], None, [256], [0,256]) #frequency of each pixel intensity value
+                  prob = histogram/image_pixels
+                 
+                 #Shannon Entropy
+                  for p in prob.flatten():
+                       if p > 0:
+                            entropy -= p * np.log2(p)
 
+            print("Average Entropy: ")
+            print(entropy/ 20.0)
+                
+    
+
+                 
+    
  
 def main():
     createImages()
+  
     
 if __name__ == "__main__":
     main()
