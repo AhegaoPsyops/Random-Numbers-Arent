1eb077f09e3945f4826058fc2460885d0000babb
Josie Benson <jbenson6@cub.uca.edu>
2025-11-24 05:23:57 -0600

    analyzeImages refactoring

--- Encryption/Encrypt.py
+++ Encryption/Encrypt.py
@@ -8,6 +8,64 @@ import time
 import random as rand
 import hashlib
 
+################ VARIABLES ###################
+DESIRED_WIDTH = 480
+DESIRED_HEIGHT = 640
+ANALYZE_IMG_ORD = 'q'
+IMAGE_TAKER_ORD = 32
+CLEAR_IMAGES_ORD = 'c'
+
+########## CREATE_FOLDER ######################
+### Creates the folder for the photos to be saves in
+### Parameters ###
+# name - the desired name of the folder
+### Returns ###
+# folder - the name of the folder
+def create_folder(name):
+    #Creates a folder for saved photos if captured_photos is not there already 
+        #(default: it should be there)
+        folder = name
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        return folder
+
+############   SPACE_BAR_COMMAND   ######################
+### If space bar, then take photos
+### Parameters ###
+# num_photos - number of photos to be taken
+# capture - capture video instance
+# save_folder - name of the folder to be saved to 
+# delay - defined time delay for photos
+# counter - for loop/name of file
+### Returns ###
+# counter - updated counter
+def image_taker(num_photos, capture, save_folder, delay, counter):
+    for i in range(num_photos):
+        ret, frame = capture.read() #Reads frame
+
+        if not ret:
+            print("Read error while capturing photos!")
+            break
+
+        img_name = os.path.join(save_folder, f"opencv_frame_{counter}.png")  #Names image  and joins it to the save folder (captured_images)
+        cv.imwrite(img_name, frame)  #Writes the image into save folder
+
+        print("{} written!".format(img_name))  #Debugging  #Debugging
+        counter += 1  #Adds to image counter for naming purposes.
+        time.sleep(delay) #Delay with photos being taken.
+
+    return counter  # return updated counter
+
+################ CLEAR_IMAGES #################
+### If C used on keyboard, then delete images from the image folder directory
+### Parameters ###
+# save_folder - the name of the file 
+### Returns ###
+# N/A
+def clear_images(save_folder):
+    for fileName in os.listdir(save_folder):
+        if fileName.startswith("opencv_frame_") and fileName.endswith(".png"):
+            os.remove(os.path.join(save_folder, fileName))
 
 #Starts up OpenCV webcam, takes photos from the webcam that will be used to collect data from. 
 def createImages():
@@ -18,20 +76,14 @@ def createImages():
         #Timer for photos being taken after each iteration
         delay = 0.5
 
-        #Creates a folder for saved photos if captured_photos is not there already 
-        #(default: it should be there)
-        save_folder = "captured_images"
-        if not os.path.exists(save_folder):
-            os.makedirs(save_folder)
+        ##create the save folder
+        save_folder = create_folder("captured_images")
         
         #Captures video
         cap = cv.VideoCapture(0)
 
-        desired_width = 480
-        desired_height = 640
-
-        cap.set(cv.CAP_PROP_FRAME_WIDTH, desired_width)
-        cap.set(cv.CAP_PROP_FRAME_HEIGHT, desired_height)
+        cap.set(cv.CAP_PROP_FRAME_WIDTH, DESIRED_WIDTH)
+        cap.set(cv.CAP_PROP_FRAME_HEIGHT, DESIRED_HEIGHT)
       
         #While video is playing...
         while True:
@@ -42,37 +94,24 @@ def createImages():
             if not ret:
                 print("Error!")
                 break
-       
         
-        # Display the resulting frame (live feed from camera)
+            # Display the resulting frame (live feed from camera)
             cv.imshow('Frame_final', frame)
+            key = cv.waitKey(1) & 0xFF  # READ KEY ONCE
 
-        #If the user presses the space bar, it takes PNG images of the frame being captured via webcam.
-            if cv.waitKey(1) % 256 == 32:
-                for i in range(numberOfPhotos):
-                     ret, frame = cap.read() #Reads frame
-                     img_name = os.path.join(save_folder, f"opencv_frame_{counter}.png") #Names image  and joins it to the save folder (captured_images)
-                     imgData = cv.imwrite(img_name, frame) #Writes the image into save folder
-                     print("{} written!".format(img_name))  #Debugging
-                     print(counter)
-                     counter += 1  #Adds to image counter for naming purposes.
-                     time.sleep(delay) #Delay with photos being taken.
-            
+            #If the user presses the space bar, it takes PNG images of the frame being captured via webcam.
+            if key == IMAGE_TAKER_ORD:
+                counter = image_taker(numberOfPhotos, cap, save_folder, delay, counter)
 
-        #If the user presses q, it analyzes images and leaves the webcam.
-            if cv.waitKey(1) == ord('q'):
+            #If the user presses q, it analyzes images and leaves the webcam.
+            elif key == ord(ANALYZE_IMG_ORD):
                 analyzeImages()
-                exit()
+                break
                 
 
-        #If the user presses c, it will clear out the captured_images directory in case the user wants to restart/retakes images.
-            if cv.waitKey(1) == ord('c'):
-                i = 0
-                for fileName in os.listdir(save_folder):
-                     if fileName.endswith('.png'):
-                         os.remove(os.path.join(save_folder, f"opencv_frame_{i}.png"))
-                         i+= 1
-               
+            #If the user presses c, it will clear out the captured_images directory in case the user wants to restart/retakes images.
+            if key == ord(CLEAR_IMAGES_ORD):
+                clear_images(save_folder)
         
         cap.release()
         cv.destroyAllWindows
@@ -91,8 +130,8 @@ def analyzeImages():
                 img_name = os.path.join(save_folder, f"opencv_frame_{counter}.png") 
                 imgData = cv.imread(img_name, cv.IMREAD_COLOR_RGB)
                 height, width, channels = imgData.shape #Gets the dimensions of the PNG
-                randRow = rand.randint(0, height) #Randomize the row pixel value that will be evaluated
-                randCol = rand.randint(0, width) #Randomize the col pixel value that will be evaluated
+                randRow = rand.randint(0, height - 1) #Randomize the row pixel value that will be evaluated
+                randCol = rand.randint(0, width - 1) #Randomize the col pixel value that will be evaluated
                 p_value = (imgData[randRow, randCol]) #Stores BGR image data
                 blueValue = p_value[0] #Blue value
                 redValue = p_value[1] #Red value
@@ -142,7 +181,10 @@ def entropy():
     
  
 def main():
-    createImages()
+    try:
+        createImages()
+    except KeyboardInterrupt:
+        print("\n[!] Program stopped by user.")
   
     
 if __name__ == "__main__":
--- Encryption/test_Encrypt.py
+++ Encryption/test_Encrypt.py
@@ -5,13 +5,20 @@ def test_createImages():
     #TODO
     return
 
-def test_webcam_opens(capsys):
+def test_Encrypt(capsys):
+    ########### ENSURE WEBCAM OPENS WITH OUTPUT ########################
     ##run created images to get output
     createImages()
     ##captures the output in terminal
-    captured = capsys.readouterr()  
+    captured = capsys.readouterr()
     ##asserts correct message is outputed that proves the webcam is open
     assert captured == "Webcam opened successfully."
+
+    ########### ENSURE ANALYZE_IMAGE RUNS CORRECTLY #####################
+    def test_analyzeImages(capsys):
+        analyzeImages()
+        captured = capsys.readouterr()
+        print(captured)
 
 def test_analyzeImages():
     #TODO
